d4f15d16ed72a114f475bf2630e012f557c1e1c8
Jacek <j.c.wojdel@gmail.com>
2017-02-24 08:57:40 +0100

    A lot of changes related to playing songs

--- lambdas/my_pi_lambda.py
+++ lambdas/my_pi_lambda.py
@@ -5,19 +5,25 @@ import json
 import base64
 import os
 
-SECRET = os.environ['SECRET']
-HOST = os.environ['HOST']
+SECRET = os.environ.get('SECRET', 'no secret for tests')
+HOST = os.environ.get('HOST', 'no host for tests')
 
 
 def get_intent(event):
-    if event['request']['type'] != 'IntentRequest':
-        raise ValueError('Event does not correspond to intent request', event)
-    return event['request']['intent']['name'].replace('.', '')
+    etype = event['request']['type']
+    if etype == 'IntentRequest':
+        return event['request']['intent']['name'].replace('.','')
+    elif etype == 'LaunchRequest':
+        return 'LaunchRequest'
+    elif etype == 'SessionEndedRequest':
+        return 'SessionEndedRequest'
+    else:
+        raise Exception('Unknown event type', event)
 
 
 def get_slots(event):
     slots = {}
-    event_slot_dict = event['request']['intent'].get('slots', {})
+    event_slot_dict = event['request'].get('intent', {}).get('slots', {})
 
     for slot_name, slot_dict in event_slot_dict.items():
         slots[slot_name] = slot_dict.get('value', None)
@@ -38,8 +44,7 @@ class RadioController(object):
         if 1 <= channel_num <= 4:
             url = 'http://stream3.polskieradio.pl:890{}/listen.pls'.format(2 * (channel_num - 1))
         else:
-            return self.ask_for_details('I dont know this radio. Which other Polish radio would you like to play?',
-                                        session_attr={'Context': 'Radio'})
+            return self.ask_for_details('I dont know this radio. Which other Polish radio would you like to play?', session_attr={'Context': 'Radio'})
 
         self.request_method('Player.Open', item={'file': url})
 
@@ -47,9 +52,11 @@ class RadioController(object):
 
     def AMAZONStopIntent(self):
         active_players = self.request_method('Player.GetActivePlayers')
-        self.request_method('Player.Stop', playerid=active_players[0]['playerid'])
-
-        return self.respond_simple_text('Stopped playing radio')
+        if active_players:
+            self.request_method('Player.Stop', playerid=active_players[0]['playerid'])
+            return self.respond_simple_text('Stopped playing')
+        else:
+            return self.respond_simple_text('Your Pi does not seem to play anything')
 
     def SetVolumeIntent(self, Number):
         if Number is None:
@@ -60,15 +67,8 @@ class RadioController(object):
 
         return self.respond_simple_text('Volume set to {}'.format(Number))
 
-    def AmbiguousNumberIntent(self, Number):
-        context = self.session_attributes.get('Context')
-        if context == 'Radio':
-            return self.PlayPolishRadioIntent(Number)
-        if context == 'Volume':
-            return self.SetVolumeIntent(Number)
 
-        return self.ask_for_details('I did not understand what you meant. What do you want my pie to do?')
-
+class PlayerController(object):
     def VolumeUpIntent(self):
         current_properties = self.request_method('Application.GetProperties', properties=['volume'])
         current_volume = int(current_properties['volume'])
@@ -99,6 +99,54 @@ class RadioController(object):
         self.request_method('Application.SetMute', mute=False)
 
         return self.respond_simple_text('Radio unmuted')
+
+    def LaunchRequest(self):
+        return self.ask_for_details('What would you like your pie to play for you?')
+
+    def SessionEndedRequest(self):
+        return self.respond_simple_text('Bye')
+
+
+class MediaController(object):
+    def PlayArtistIntent(self, Artist):
+        artist_response = self.request_method('AudioLibrary.GetArtists')
+
+        artist_id = None
+        for artist in artist_response['artists']:
+            if artist['artist'].lower() == Artist.lower():
+                artist_id = artist['artistid']
+
+        if artist_id is None:
+            return self.respond_simple_text('I could not find songs by {}'.format(Artist))
+
+        self.request_method('Playlist.Clear', playlistid=0)
+        self.request_method('Playlist.Add', item={'artistid': artist_id}, playlistid=0)
+
+        self.request_method('Player.Open', item={'playlistid': 0})
+        self.request_method('Player.SetShuffle', shuffle=True, playerid=0)
+        return self.respond_simple_text('Playing songs by {} at random'.format(Artist))
+
+    def PlayAlbumIntent(self, Album):
+        album_response = self.request_method('AudioLibrary.GetAlbums')
+
+        album_id = None
+        for album in album_response['albums']:
+            if album['label'].lower() == Album.lower():
+                album_id = album['albumid']
+
+        if album_id is None:
+            return self.respond_simple_text('I could not find album {}'.format(Album))
+
+        self.request_method('Playlist.Clear', playlistid=0)
+        self.request_method('Playlist.Add', item={'albumid': album_id}, playlistid=0)
+
+        self.request_method('Player.Open', item={'playlistid': 0})
+        self.request_method('Player.SetShuffle', shuffle=False, playerid=0)
+        return self.respond_simple_text('Playing album {}'.format(Album))
+
+    def ScanIntent(self):
+        self.request_method('AudioLibrary.Scan')
+        return self.respond_simple_text('Scanning library for new songs')
 
 
 class SkillController(object):
@@ -160,10 +208,9 @@ class PiController(object):
         password = self.build_current_password(offset)
         auth = base64.b64encode('kodi:{}'.format(password))
         headers = {
-            'Content-type': 'application/json',
-            'Accept': 'application/json',
-            'Authorization': 'Basic {}'.format(auth)
-        }
+            'Content-type':'application/json',
+            'Accept':'application/json',
+            'Authorization':'Basic {}'.format(auth)}
         conn = httplib.HTTPConnection(HOST)
         conn.request('POST', '/jsonrpc', json.dumps(data), headers)
         response = conn.getresponse()
@@ -195,8 +242,16 @@ class PiController(object):
         raise Exception('Failure to communicate with PI: {} - {}'.format(response.status, response.reason))
 
 
-class MyPiRadioSkill(RadioController, PiController, SkillController):
-    pass
+class MyPiRadioSkill(RadioController, PlayerController, MediaController, PiController, SkillController):
+
+    def AmbiguousNumberIntent(self, Number):
+        context = self.session_attributes.get('Context')
+        if context == 'Radio':
+            return self.PlayPolishRadioIntent(Number)
+        if context == 'Volume':
+            return self.SetVolumeIntent(Number)
+
+        return self.ask_for_details('I did not understand what you meant. What do you want my pie to do?')
 
 
 def lambda_handler(event, context):
